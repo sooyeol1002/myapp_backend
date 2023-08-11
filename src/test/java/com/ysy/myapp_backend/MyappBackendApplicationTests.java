881d642d352f1fcb750a9560fdccc989a8e77f90
Sooyeol Yang <didtnduf2000@gmail.com>
2023-08-11 10:40:33 +0900

    git 연동

---
+++ src/test/java/com/ysy/myapp_backend/MyappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ysy.myapp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
